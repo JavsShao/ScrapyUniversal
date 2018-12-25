1e37c4e58094ef8af622dd918640692b0cfaf56e
hamdi <javs_shao@163.com>
2018-12-25 10:20:09 +0800

    解析页面_parse_item

--- scrapyuniversal/scrapyuniversal/spiders/china.py
+++ scrapyuniversal/scrapyuniversal/spiders/china.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from scrapyuniversal.items import *
@@ -20,5 +19,11 @@ class ChinaSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        loader = ChinaSpider(item=NewsItem(), response=response)
-        loader.add_xpath('title',)
+        loader = ChinaLoader(item=NewsItem(), response=response)
+        loader.add_xpath('title', '//h1[@id="chan_newsTitle"]/text()')
+        loader.add_value('url', response.url)
+        loader.add_xpath('text', '//div[@id="chan_newsDetail"]//text()')
+        loader.add_xpath('datetime', '//div[@id="chan_newsInfo"]/text()', re='(\d+-\d+-\d+\s\d+:\d+:\d+)')
+        loader.add_xpath('source', '//div[@id="chan_newsInfo"]/text()', re='来源：(.*)')
+        loader.add_value('website', '中华网')
+        yield loader.load_item()
