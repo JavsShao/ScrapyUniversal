a4bd08a796554abc35979dee1a4aa043c2438c0b
hamdi <javs_shao@163.com>
2018-12-25 11:20:29 +0800

    代码完善_2

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import sys
 from scrapy.utils.project import get_project_settings
-from scrapyuniversal.scrapyuniversal.spiders.universal import UniversalSpider
-from scrapyuniversal.scrapyuniversal.utils import get_config
+from scrapyuniversal.spiders.universal import UniversalSpider
+from scrapyuniversal.utils import get_config
 from scrapy.crawler import CrawlerProcess
 
 def run():
